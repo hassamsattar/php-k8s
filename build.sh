c6f649c1070aef83450b0e2561267bada0daeb6b
root <root@hassam>
2019-09-24 09:03:11 -0400

    Adding docker-compose & build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+if [[ "$(docker images -q hassam1989/php-alpine:latest 2> /dev/null)" == "" ]]; then
+  docker build -f docker/DockerPhpBuild -t hassam1989/php-alpine:latest .
+fi
+
+
+rm -rf node_modules
+docker run -w /app -v $(pwd)/hassam-docker/:/app node npm install
+docker run -w /app -v $(pwd)/hassam-docker/:/app node npm run dev
+docker build -f docker/DockerPhpLive -t hassam1989/php-live:latest ./hassam-docker/
+docker build -f docker/DockerNginx -t hassam1989/php-nginx:latest ./hassam-docker/
+docker push hassam1989/php-live:latest
+docker push hassam1989/php-nginx:latest
